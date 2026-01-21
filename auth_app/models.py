fc4b5007ef04cd1e9770f828c3b1007254cf8f27
alexschoefer <alex.schoefer@gmx.net>
2026-01-21 20:18:56 +0100

    feat: add first model UserProfile in auth_app

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    #Ein Account gehört genau zu einem User und ein User hat genau einen Account
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.fullname
