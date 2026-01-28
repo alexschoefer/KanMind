c76d46e19caae2b0d5449041799db33891568a30
alexschoefer <alex.schoefer@gmx.net>
2026-01-28 22:14:01 +0100

    refactor: has_object_permission in board_app

--- board_app/api/permissions.py
+++ board_app/api/permissions.py
@@ -1,10 +1,18 @@
 from rest_framework import permissions
 from board_app.models import Board
 
+
 class IsBoardMemberOrOwner(permissions.BasePermission):
 
-    def has_object_permission(self, request, view, obj: Board):
-        return obj.owner == request.user or request.user in obj.members.all()
-    
+    # def has_object_permission(self, request, view, obj: Board):
+    #     return obj.owner == request.user or request.user in obj.members.all()
+
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        return (
+            obj.owner_id == user.id
+            or obj.members.filter(id=user.id).exists()
+        )
+
     def has_permission(self, request, view):
         return request.user and request.user.is_authenticated
