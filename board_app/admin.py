67d6a276c7506fe36b8e462d22f8393a4513e475
alexschoefer <alex.schoefer@gmx.net>
2026-02-07 18:16:43 +0100

    feat: add register of board_app and tasks_app in admin console

--- board_app/admin.py
+++ board_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Board
 
 # Register your models here.
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "owner")
+    search_fields = ("title", "owner__username", "owner__email")
+    filter_horizontal = ("members",) 
+
+admin.site.register(Board, BoardAdmin)
--- task_app/admin.py
+++ task_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Task, TaskCommentModel
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "board", "status", "priority", "assignee", "reviewer", "due_date")
+    list_filter = ("status", "priority", "board")
+    search_fields = ("title", "description", "assignee__username", "reviewer__username")
+
+
+@admin.register(TaskCommentModel)
+class TaskCommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "task", "author", "created_at")
+    search_fields = ("task__title", "author__username", "content")
